3c767955d500ddf67f77af27cd7aad8beef8e089
Gabriela Wassing <gabriela.wassing@gmail.com>
2024-07-31 14:22:53 +0200

    Fix allowed_hosts

--- ecommerce_platform/settings.py
+++ ecommerce_platform/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['gabys-ecommerce-platform-02f73fa9289a', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
