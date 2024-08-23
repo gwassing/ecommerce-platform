9b81e54b548480fe795384acb5184222ca3fafce
Gabriela Wassing <gabriela.wassing@gmail.com>
2024-08-23 18:32:27 +0200

    Create Product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Product (models.Model):
+    category = models.CharField(max_length=50)
+    brand = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    date_published = models.DateField(auto_now_add=True)
+
