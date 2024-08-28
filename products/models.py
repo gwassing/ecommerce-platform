7b602854bdec6815abb8d6dc16e9332896b4a3b7
Gabriela Wassing <gabriela.wassing@gmail.com>
2024-08-28 15:30:06 +0200

    Redefine fields and add string method

--- products/models.py
+++ products/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from datetime import date
 
 
 class Product (models.Model):
     category = models.CharField(max_length=50)
     brand = models.CharField(max_length=50)
-    name = models.CharField(max_length=100)
+    item_name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    date_published = models.DateField(auto_now_add=True)
+    date_created = models.DateField(default=date.today)
+
+    def __str__(self):
+        return self.item_name
 
