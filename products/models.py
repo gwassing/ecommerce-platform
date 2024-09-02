b81c825005b6391843068d49862413018840de6e
Gabriela Wassing <gabriela.wassing@gmail.com>
2024-09-02 15:14:34 +0200

    Define category field as choices

--- products/models.py
+++ products/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 from datetime import date
 
+PRODUCT_CATEGORIES = {
+    "SHOES": "Shoes",
+    "CLOTHING": "Clothing",
+    "ACCESSORIES": "Accessories"
+}
+
 
 class Product (models.Model):
-    category = models.CharField(max_length=50)
+    category = models.CharField(max_length=50, choices=PRODUCT_CATEGORIES)
     brand = models.CharField(max_length=50)
     item_name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=5, decimal_places=2)
