c26bb6bc3d3689f32ea4ed326aaa024cc7fd9214
Gabriela Wassing <gabriela.wassing@gmail.com>
2024-11-08 19:07:31 +0100

    Add product list page

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.product_list, name="index"),
+    path("", views.ProductListView.as_view(), name="product_list"),
     path("<int:pk>", views.ProductDetailView.as_view(), name="product_detail")
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
 from products.models import Product
 
@@ -9,10 +9,13 @@ def index(request):
     return HttpResponse("hello world")
 
 
-def product_list(request):
-    return HttpResponse("list of products")
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/product_list.html'
+    context_object_name = "products"
 
 
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'products/product_detail.html'
+
