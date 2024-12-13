d7ca955e5fc4d5f4f27d37174934ea86984f4f7a
Gabriela Wassing <gabriela.wassing@gmail.com>
2024-12-13 16:36:33 +0100

    Automatically login user after succesfull registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,17 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
+from django.contrib.auth import login
+
 from django.urls import reverse_lazy
 
 
 class RegistrationView(CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy("login")
+    success_url = reverse_lazy("index")
     template_name = "registration/registration.html"
+
+    def form_valid(self, form):
+        if form.is_valid():
+            response = super().form_valid(form)
+            login(self.request, self.object)
+            return response
