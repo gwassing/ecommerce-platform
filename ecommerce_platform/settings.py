fadde2121a70e658eccf048a2f771fbcdee832df
Gabriela Wassing <gabriela.wassing@gmail.com>
2025-01-10 10:30:41 +0100

    Add shell plus

--- ecommerce_platform/settings.py
+++ ecommerce_platform/settings.py
@@ -45,8 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products',
+    'django_extensions',
     'storages',
+    'products',
     'accounts',
 ]
 
