e5dbfee7a38f21a2e9fa9bc469318856410efece
Gabriela Wassing <gabriela.wassing@gmail.com>
2025-03-21 17:19:02 +0100

    Add quantity field to product detail page

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import DetailView, ListView
 
 from products.models import Product
+from cart.forms import AddItemToCartForm
 
 
 class ProductListView(ListView):
@@ -16,4 +17,5 @@ class ProductDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["images"] = self.object.images.all()
+        context['cart_form'] = AddItemToCartForm()
         return context
