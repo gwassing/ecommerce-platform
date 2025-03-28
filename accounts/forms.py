19345523c841bb3e4a12b4b36eca7f49b6b1bbe9
Gabriela Wassing <gabriela.wassing@gmail.com>
2025-03-28 14:43:44 +0100

    Overwrite default user model in registration form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
 
 class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'password1', 'password2')
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
