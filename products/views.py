0d9ceee98cbda89bfa92067697624a3816ff0ff0
Gabriela Wassing <gabriela.wassing@gmail.com>
2025-04-04 17:03:05 +0200

    Add redis caching

--- products/views.py
+++ products/views.py
@@ -1,15 +1,19 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from django.views.generic import DetailView, ListView
 
 from products.models import Product
 from cart.forms import AddItemToCartForm
 
 
+@method_decorator(cache_page(60 * 15), name='dispatch')
 class ProductListView(ListView):
     model = Product
     template_name = 'products/product_list.html'
     context_object_name = "products"
 
 
+@method_decorator(cache_page(60 * 15), name='dispatch')
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'products/product_detail.html'
