b5c83c02a07621c130425bcabc7b48a6524b8b77
Gabriela Wassing <gabriela.wassing@gmail.com>
2025-05-09 17:02:31 +0200

    Implement data-level caching instead of full page caching

--- products/views.py
+++ products/views.py
@@ -1,19 +1,30 @@
-from django.utils.decorators import method_decorator
-from django.views.decorators.cache import cache_page
+from django.core.cache import cache
 from django.views.generic import DetailView, ListView
 
 from products.models import Product
 from cart.forms import AddItemToCartForm
 
 
-@method_decorator(cache_page(60 * 15), name='dispatch')
 class ProductListView(ListView):
     model = Product
     template_name = 'products/product_list.html'
     context_object_name = "products"
+    cache_timeout = 60*60*24
+
+    def get_queryset(self):
+        cache_key = "product_list_queryset"
+        queryset = cache.get(cache_key)
+
+        if queryset is None:
+            queryset = super().get_queryset()
+            cache.set(cache_key, queryset, self.cache_timeout)
+            print('cache miss')
+        else:
+            print('cache hit')
+
+        return queryset
 
 
-@method_decorator(cache_page(60 * 15), name='dispatch')
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'products/product_detail.html'
