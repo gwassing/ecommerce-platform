fa7d20e29b1c0732427986cf26683d22cf2a547a
Gabriela Wassing <gabriela.wassing@gmail.com>
2025-06-17 17:49:24 +0200

    Add sorting for cart items in cart

--- cart/models.py
+++ cart/models.py
@@ -21,8 +21,12 @@ class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, null=False, related_name='cart_items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False)
     quantity = models.PositiveIntegerField(default=1)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.product.item_name
+
+    class Meta:
+        ordering = ["-created"]
 
 
