a7c5411c0210e9d9ccd25882c3d52e49685fa8f9
Gabriela Wassing <gabriela.wassing@gmail.com>
2025-06-25 13:59:51 +0200

    Replace statuses dict with Status class for StatusMixin choices

--- core/models.py
+++ core/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 
 
-STATUSES = {
-    "ARCHIVED": "Archived",
-    "ACTIVE": "Active"
-}
+class Status(models.TextChoices):
+    ARCHIVED = "archived", "Archived"
+    ACTIVE = "active", "Active"
 
 
 class StatusMixin(models.Model):
-    status = models.CharField(max_length=20, choices=STATUSES)
+    status = models.CharField(max_length=20, choices=Status, default=Status.ACTIVE)
 
     class Meta:
         abstract = True
