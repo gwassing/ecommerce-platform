30d2273981d112a5ced9f0aeee51c596262eb8a1
Gabriela Wassing <gabriela.wassing@gmail.com>
2025-08-01 17:28:25 +0200

    Use default (or last used) shipping details if set, else show form

--- checkout/views.py
+++ checkout/views.py
@@ -4,8 +4,7 @@ from django.urls import reverse
 from django.views import generic
 
 from accounts.forms import ShippingDetailsForm
-from accounts.models import Order, PurchasedItem
-from cart.models import Cart
+from accounts.models import Order, PurchasedItem, ShippingDetails
 
 
 class CheckoutView(LoginRequiredMixin, generic.TemplateView):
@@ -13,44 +12,67 @@ class CheckoutView(LoginRequiredMixin, generic.TemplateView):
 
     def get_context_data(self, **kwargs):
         cart = self.request.user.cart
+        default_shipping_address = ShippingDetails.objects.filter(
+            user=self.request.user,
+            is_default=True
+        )
+
+        last_used_shipping_address = ShippingDetails.objects.filter(user=self.request.user)
+
         return super().get_context_data(**kwargs) | {
             "cart_items": cart.get_items(),
             "total_price": cart.get_total_price(),
-            "shipping_form": ShippingDetailsForm()
+            "shipping_form": ShippingDetailsForm(),
+            "default_shipping_address": default_shipping_address.first() if default_shipping_address.exists() else None,
+            "last_used_shipping_address": last_used_shipping_address.last() if last_used_shipping_address.exists() else None,
         }
 
     def post(self, request, *args, **kwargs):
-        form = ShippingDetailsForm(data=request.POST)
+        action = request.POST.get('action')
 
-        if form.is_valid():
-            # attach user to shipping details form instance
-            shipping_details_obj = form.save(commit=False)
-            shipping_details_obj.user = request.user
+        if action == 'create_new_shipping_address':
+            form = ShippingDetailsForm(data=request.POST)
 
-            # create an order with the user's shipping details
-            order = Order.objects.create(
-                user=request.user,
-                shipping_details=shipping_details_obj
+            if form.is_valid():
+                # attach user to shipping details form instance
+                shipping_details_obj = form.save(commit=False)
+                shipping_details_obj.user = request.user
+                shipping_details_obj.save()
+            else:
+                # If the form is invalid, get the standard context and then add the invalid form object to it.
+                context = self.get_context_data()
+                context['shipping_form'] = form
+                return render(request, self.template_name, context)
+
+        elif action == 'use_default_shipping_address':
+            default_shipping_address = ShippingDetails.objects.filter(
+                user=self.request.user,
+                is_default=True
+            ).first()
+            shipping_details_obj = default_shipping_address
+
+        elif action == 'use_last_used_shipping_address':
+            last_used_shipping_address = ShippingDetails.objects.filter(user=self.request.user).last()
+            shipping_details_obj = last_used_shipping_address
+
+        # create an order with the user's shipping details
+        order = Order.objects.create(
+            user=request.user,
+            shipping_details=shipping_details_obj
+        )
+
+        # transform user's cart items into purchased items and add to order object
+        for item in request.user.cart.cart_items.all():
+            PurchasedItem.objects.create(
+                order=order,
+                product=item.product,
+                quantity=item.quantity
             )
 
-            # transform user's cart items into purchased items and add to order object
-            for item in request.user.cart.cart_items.all():
-                PurchasedItem.objects.create(
-                    order=order,
-                    product=item.product,
-                    quantity=item.quantity
-                )
+        # empty cart
+        request.user.cart.cart_items.all().delete()
 
-            # empty cart
-            request.user.cart.cart_items.all().delete()
-
-            return redirect(reverse('checkout:order_confirmation'))
-
-        # context is created so that form is displayed with fields pre-filled in case form is not valid
-        context = {
-            'form': form
-        }
-        return render(request, self.template_name, context)
+        return redirect(reverse('checkout:order_confirmation'))
 
 
 class OrderConfirmationView(LoginRequiredMixin, generic.TemplateView):
