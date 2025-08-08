e6dc9f9287566965a75b000738f92dc26a0d8aa4
Gabriela Wassing <gabriela.wassing@gmail.com>
2025-08-08 18:51:17 +0200

    Simpler way to place order for existing shipping details

--- checkout/urls.py
+++ checkout/urls.py
@@ -6,5 +6,6 @@ app_name = 'checkout'
 
 urlpatterns = [
     path('', views.CheckoutView.as_view(), name="checkout"),
+    path('<int:pk>/', views.CheckoutExistingShippingDetailsView.as_view(), name="checkout_existing_shipping_details"),
     path('confirmation/', views.OrderConfirmationView.as_view(), name="order_confirmation")
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -31,27 +31,18 @@ class CheckoutView(LoginRequiredMixin, generic.TemplateView):
         }
 
     def post(self, request, *args, **kwargs):
-        action = request.POST.get('action')
+        form = ShippingDetailsForm(data=request.POST)
 
-        if action == 'create_new_shipping_details':
-            form = ShippingDetailsForm(data=request.POST)
-
-            if form.is_valid():
-                # attach user to shipping details form instance
-                shipping_details_obj = form.save(commit=False)
-                shipping_details_obj.user = request.user
-                shipping_details_obj.save()
-            else:
-                # If the form is invalid, get the standard context and then add the invalid form object to it.
-                context = self.get_context_data()
-                context['shipping_form'] = form
-                return render(request, self.template_name, context)
-        elif action == 'use_existing_shipping_details':
-            existing_shipping_details = ShippingDetails.objects.filter(
-                user=self.request.user,
-                is_default=True
-            ).first() or ShippingDetails.objects.filter(user=self.request.user).last()
-            shipping_details_obj = existing_shipping_details
+        if form.is_valid():
+            # attach user to shipping details form instance
+            shipping_details_obj = form.save(commit=False)
+            shipping_details_obj.user = request.user
+            shipping_details_obj.save()
+        else:
+            # If the form is invalid, get the standard context and then add the invalid form object to it.
+            context = self.get_context_data()
+            context['shipping_form'] = form
+            return render(request, self.template_name, context)
 
         # create an order with the user's shipping details
         order = Order.objects.create(
@@ -70,6 +61,37 @@ class CheckoutView(LoginRequiredMixin, generic.TemplateView):
         # empty cart
         request.user.cart.cart_items.all().delete()
 
+        return redirect(reverse('checkout:order_confirmation'))
+
+
+class CheckoutExistingShippingDetailsView(LoginRequiredMixin, generic.View):
+    def post(self, request, *args, **kwargs):
+        try:
+            default_shipping_details = ShippingDetails.objects.get(
+                user=self.request.user,
+                is_default=True
+            )
+        except ShippingDetails.DoesNotExist:
+            default_shipping_details = None
+
+        last_used_shipping_details = Order.objects.latest('pk').shipping_details
+        shipping_details_obj = default_shipping_details if default_shipping_details else last_used_shipping_details
+
+        order = Order.objects.create(
+            user=request.user,
+            shipping_details=shipping_details_obj
+        )
+
+        # transform user's cart items into purchased items and add to order object
+        for item in request.user.cart.cart_items.all():
+            PurchasedItem.objects.create(
+                order=order,
+                product=item.product,
+                quantity=item.quantity
+            )
+
+        # empty cart
+        request.user.cart.cart_items.all().delete()
         return redirect(reverse('checkout:order_confirmation'))
 
 
