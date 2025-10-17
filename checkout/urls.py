c0cf46b8fdd1d6af201dc764a9d8123dedc597a4
Gabriela Wassing <gabriela.wassing@gmail.com>
2025-10-17 14:35:56 +0200

    Handle order placing in CheckoutShippingDetailsSelectView
    ... instead of in CreateOrderMixin.
    
    Drop CreateOrderMixin and ShippingDetailsRedirectView, these are not necessary anymore after re-designing the checkout flow.

--- checkout/urls.py
+++ checkout/urls.py
@@ -7,6 +7,5 @@ app_name = 'checkout'
 urlpatterns = [
     path("address/", views.CheckoutShippingDetailsSelectView.as_view(), name="address"),
     path("payment/", views.CheckoutPaymentView.as_view(), name="payment"),
-    path('<int:pk>/', views.ExistingShippingDetailsRedirectView.as_view(), name="existing_shipping_details_redirect"),
     path('confirmation/', views.OrderConfirmationView.as_view(), name="order_confirmation")
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -4,29 +4,7 @@ from django.urls import reverse
 from django.views import generic
 
 from accounts.forms import ShippingDetailsSelectForm
-from accounts.models import Order, PurchasedItem, ShippingDetails
-
-
-class CreateOrderMixin:
-    def create_order_and_clear_cart(self, shipping_details):
-        user = self.request.user
-        # create an order with the user's shipping details
-        order = Order.objects.create(
-            user=user,
-            shipping_details=shipping_details
-        )
-
-        # transform user's cart items into purchased items and add to order object
-        for item in user.cart.cart_items.all():
-            PurchasedItem.objects.create(
-                order=order,
-                product=item.product,
-                quantity=item.quantity
-            )
-
-        # empty cart
-        user.cart.cart_items.all().delete()
-        return reverse('checkout:order_confirmation')
+from accounts.models import Order, PurchasedItem
 
 
 class CheckoutShippingDetailsSelectView(LoginRequiredMixin, generic.FormView):
@@ -42,23 +20,39 @@ class CheckoutShippingDetailsSelectView(LoginRequiredMixin, generic.FormView):
         selected_address = form.cleaned_data['shipping_details']
         # Save to Django session
         self.request.session['checkout_address_id'] = selected_address.id
-        print(f"✅ Stored address ID {selected_address.id} in session")
+        print('selected_address:', selected_address)
 
-        return redirect('checkout:payment')
+        # add error validation for no selected address
+
+        return redirect(self.place_order_and_clear_cart())
+
+    def place_order_and_clear_cart(self):
+        selected_address_id = self.request.session.get('checkout_address_id')
+
+        user = self.request.user
+        # create an order with the user's shipping details
+        order = Order.objects.create(
+            user=user,
+            shipping_details_id=selected_address_id
+        )
+
+        # transform user's cart items into purchased items and add to order object
+        for item in user.cart.cart_items.all():
+            PurchasedItem.objects.create(
+                order=order,
+                product=item.product,
+                quantity=item.quantity
+            )
+
+        print('purchased items:', order.purchased_items.all())
+
+        # empty cart
+        user.cart.cart_items.all().delete()
+        return reverse('checkout:order_confirmation')
 
 
 class CheckoutPaymentView(LoginRequiredMixin, generic.TemplateView):
     template_name = 'checkout/checkout_payment.html'
-
-
-class ExistingShippingDetailsRedirectView(LoginRequiredMixin, CreateOrderMixin, generic.RedirectView):
-    def get_redirect_url(self, *args, **kwargs):
-        shipping_details_obj = ShippingDetails.objects.get(
-            pk=kwargs['pk'],
-            user=self.request.user
-        )
-
-        return self.create_order_and_clear_cart(shipping_details_obj)
 
 
 class OrderConfirmationView(LoginRequiredMixin, generic.TemplateView):
